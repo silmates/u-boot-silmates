7bf871704e31d273213361473b96d17bdabe4153
mitesh.mahera <mitesh.mahera@silmates.com>
2026-05-07 15:16:20 +0530

    Update imx8mm_mez.h file for set logo in center

--- include/configs/imx8mm_mez.h
+++ include/configs/imx8mm_mez.h
@@ -140,6 +140,7 @@
 	"bsp_script=boot.scr\0" \
 	"image=Image\0" \
 	"splashimage=0x50000000\0" \
+	"splashpos=m,m\0" \
 	"console=ttymxc3,115200\0" \
 	"fdt_addr_r=0x43000000\0"			\
 	"fdt_addr=0x43000000\0"			\
